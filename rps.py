abf9fbca13b1b74356cfad4ed1e80101a1723840
tooltime <tooltime@mohawkcollege.ca>
2025-04-02 16:14:19 -0400

    added repeated play functionality to code

--- rps.py
+++ rps.py
@@ -4,25 +4,33 @@
 
 import random
 
-user_action = input("Enter throw (rock, paper, scissors): ")
-ai_action = random.choice(["rock", "paper", "scissors"])
+user_choice = input("what do you want to do Play(p) or Quit(e): ")
+if user_choice == "e":
+    print("You have chosen to quit the game.")
+    exit()
+elif user_choice == "p":
 
-print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+    user_action = input("Enter throw (rock, paper, scissors): ")
+    ai_action = random.choice(["rock", "paper", "scissors"])
 
-if user_action == ai_action:
-    print(f"Both players selected {user_action}. It's a tie!")
-elif user_action == "rock":
-    if ai_action == "scissors":
-        print("Rock smashes scissors! You win!")
-    else:
-        print("Paper covers rock! You lose.")
-elif user_action == "paper":
-    if ai_action == "rock":
-        print("Paper covers rock! You win!")
-    else:
-        print("Scissors cuts paper! You lose.")
-elif user_action == "scissors":
-    if ai_action == "paper":
-        print("Scissors cuts paper! You win!")
-    else:
-        print("Rock smashes scissors! You lose.")
+    print(f"\nYou chose {user_action}, AI chose {ai_action}.\n")
+
+    if user_action == ai_action:
+        print(f"Both players selected {user_action}. It's a tie!")
+    elif user_action == "rock":
+        if ai_action == "scissors":
+            print("Rock smashes scissors! You win!")
+        else:
+            print("Paper covers rock! You lose.")
+    elif user_action == "paper":
+        if ai_action == "rock":
+            print("Paper covers rock! You win!")
+        else:
+            print("Scissors cuts paper! You lose.")
+    elif user_action == "scissors":
+        if ai_action == "paper":
+            print("Scissors cuts paper! You win!")
+        else:
+            print("Rock smashes scissors! You lose.")
+else:
+    print("Invalid input, try again.")
